599b0a037faec4730fca59b4464787bf6d2757a4
senamashira <anandhkrsna@gmail.com>
2025-11-28 07:07:11 +0800

    ketinggalan filenya anjay

---
+++ app/Http/Controllers/OpportunityCategoryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OpportunityCategory;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OpportunityCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OpportunityCategory $opportunityCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OpportunityCategory $opportunityCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, OpportunityCategory $opportunityCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OpportunityCategory $opportunityCategory)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/UserSavedOpportunityController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserSavedOpportunity;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserSavedOpportunityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserSavedOpportunity $userSavedOpportunity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UserSavedOpportunity $userSavedOpportunity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UserSavedOpportunity $userSavedOpportunity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UserSavedOpportunity $userSavedOpportunity)
+    {
+        //
+    }
+}
